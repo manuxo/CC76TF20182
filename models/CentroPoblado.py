fd4e1e506a73d88057e79f83d2311181e29b1178
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-11-18 16:08:50 -0500

    Clase Centro Poblado actualizada

--- models/CentroPoblado.py
+++ models/CentroPoblado.py
@@ -1,12 +1,13 @@
 class CentroPoblado:
-    def __init__(self,codigo,nombre,departamento,provincia,distrito,coordX, coordY):
+    def __init__(self,codigo,nombre,departamento,provincia,distrito,capital,coordX, coordY):
         self.codigo = codigo
         self.nombre = nombre
         self.departamento = departamento
         self.provincia = provincia
         self.distrito = distrito
+        self.capital = capital
         self.coordX = coordX
         self.coordY = coordY
     def __str__(self):
-        return "%s D: %s P: %s D: %s Cod: %s  X: %f Y: %f" % (self.nombre, self.departamento, self.provincia, self.distrito, self.codigo,self.coordX,self.coordY)
+        return "%s D: %s P: %s D: %s Cap: %d Cod: %s  X: %f Y: %f" % (self.nombre, self.departamento, self.provincia, self.distrito, self.capital, self.codigo,self.coordX,self.coordY)
         
